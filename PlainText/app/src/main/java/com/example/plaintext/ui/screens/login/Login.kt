2a1fd43241ffb4c647984d76920a56ffcb19c321
lucas viana <lucas.lvtorres@gmail.com>
2025-09-13 12:31:58 -0300

    FEAT(PreferencesViewModel) - Implementando Update Password, Login e Preencher

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/login/Login.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -70,7 +71,19 @@ fun Login_screen(
     navigateToList: () -> Unit,
     viewModel: PreferencesViewModel = hiltViewModel()
 ) {
-
+    Scaffold(
+        topBar = {
+            TopBarComponent(
+                navigateToSettings = { navigateToSettings() }
+            )
+        }
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+        ) { }
+    }
 }
 
 @Composable
